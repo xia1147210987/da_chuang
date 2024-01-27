54f4cfba768d7ca971f61e6fb649acb23aba1354
666fanmu <118655955+666fanmu@users.noreply.github.com>
2024-01-27 16:08:44 +0800

    6

--- djangodemo2/djangodemo2/djangoDemo2/settings.py
+++ djangodemo2/djangodemo2/djangoDemo2/settings.py
@@ -83,9 +83,9 @@ WSGI_APPLICATION = 'djangoDemo2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database_name',
+        'NAME': 'sjk',
         'USER': 'root',
-        'PASSWORD': 'xz20031024',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
